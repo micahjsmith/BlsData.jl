fa3652bf0acd05348ec59bd834a0599d0ec9e7dc
Micah Smith <micahjsmith@gmail.com>
2015-11-03 23:55:09 -0500

    Move everything to one file

--- src/BLS.jl
+++ src/BLS.jl
@@ -13,6 +13,44 @@ function BlsConnection(url=DEFAULT_API_URL; key="")
     BlsConnection(url, key)
 end
 
-include("fetch.jl")
+using Requests
+import Requests: post
+import JSON
+
+"""
+"""
+function fetch(b::BlsConnection, series::AbstractString;
+               startyear::Int=Dates.year(now())-10,
+               endyear::Int=Dates.year(now()),
+               catalog::Bool=false)
+    return fetch(b, [series]; startyear=startyear, endyear=endyear, catalog=catalog)
+end
+
+"""
+"""
+function fetch(b::BlsConnection, series::Vector{AbstractString};
+               startyear::Int=Dates.year(now())-10,
+               endyear::Int=Dates.year(now()),
+               catalog::Bool=false)
+
+    # Setup payload.
+    headers = Dict("Content-Type" => "application/json")
+    json    = Dict("seriesid"     => series,
+                   "startyear"    => startyear,
+                   "endyear"      => endyear,
+                   "catalog"      => catalog)
+
+    url     = api_url(b);
+    key     = api_key(b);
+
+    if !isempty(key)
+        json["restristrationKey"] = key
+    end
+
+    # Submit POST request to BLS
+    response = post(url; json=json, headers=headers)
+
+    return response
+end
 
 end # module
