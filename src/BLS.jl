c870f6ca54e624bccac6c32fe5800bb78b376d3d
Micah Smith <micahjsmith@gmail.com>
2015-11-03 23:51:29 -0500

    Initial progress

--- src/BLS.jl
+++ src/BLS.jl
@@ -1,5 +1,18 @@
 module BLS
 
-# package code goes here
+export BlsConnection, fetch
+
+const DEFAULT_API_URL = "http://api.bls.gov/publicAPI/v2/timeseries/data/"
+
+type BlsConnection
+    url::AbstractString
+    key::AbstractString
+end
+
+function BlsConnection(url=DEFAULT_API_URL; key="")
+    BlsConnection(url, key)
+end
+
+include("fetch.jl")
 
 end # module
