8a0b5f9a31d031fd3eb869a523e83c8d8bf22d8f
Micah Smith <micahjsmith@gmail.com>
2015-11-04 00:29:54 -0500

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using BLS
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+b0 = BlsConnection()
+b1 = BlsConnection(key="test")
+
+data = get_data(b0, "PRS85006092")
+data = get_data(b0, "PRS85006092"; catalog=true)
+data = get_data(b0, "PRS85006092"; startyear=1990, catalog=true)
