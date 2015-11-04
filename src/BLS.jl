e24eeced232d94ecab78b2ca36d81d02575b1795
Micah Smith <micahjsmith@gmail.com>
2015-11-04 00:28:15 -0500

    Request returns unparsed dict

--- src/BLS.jl
+++ src/BLS.jl
@@ -1,8 +1,15 @@
 module BLS
 
+using Requests
+import Requests: post
+import JSON
+
 export BlsConnection, get_data
 
 const DEFAULT_API_URL = "http://api.bls.gov/publicAPI/v2/timeseries/data/"
+const BLS_RESPONSE_SUCCESS = "REQUEST_SUCCEEDED"
+const BLS_RESPONSE_CATALOG_FAIL1 = "unable to get catalog data"
+const BLS_RESPONSE_CATALOG_FAIL2 = "catalog has been disabled"
 
 type BlsConnection
     url::AbstractString
@@ -16,14 +23,10 @@ end
 api_url(b::BlsConnection) = b.url
 api_key(b::BlsConnection) = b.key
 
-using Requests
-import Requests: post
-import JSON
-
 """
 """
 function get_data(b::BlsConnection, series::AbstractString;
-               startyear::Int=Dates.year(now())-10,
+               startyear::Int=Dates.year(now())-9,
                endyear::Int=Dates.year(now()),
                catalog::Bool=false)
     return get_data(b, [series]; startyear=startyear, endyear=endyear, catalog=catalog)
@@ -32,7 +35,7 @@ end
 """
 """
 function get_data{T<:AbstractString}(b::BlsConnection, series::Array{T, 1};
-               startyear::Int=Dates.year(now())-10,
+               startyear::Int=Dates.year(now())-9,
                endyear::Int=Dates.year(now()),
                catalog::Bool=false)
 
@@ -47,13 +50,34 @@ function get_data{T<:AbstractString}(b::BlsConnection, series::Array{T, 1};
     key     = api_key(b);
 
     if !isempty(key)
-        json["restristrationKey"] = key
+        json["registrationKey"] = key
     end
 
     # Submit POST request to BLS
     response = post(url; json=json, headers=headers)
+    response_json = Requests.json(response)
 
-    return response
+    # Response okay?
+    if response_json["status"] ≠ BLS_RESPONSE_SUCCESS
+        warning("Request failed with message '", response_json[status], "'")
+        return nothing
+    end
+
+    # Catalog okay?
+    catalog_okay = false
+    if catalog &&
+        !isempty(response_json["message"]) &&
+        !isempty(find(s->contains(s, BLS_RESPONSE_CATALOG_FAIL1), response_json["message"])) &&
+        !isempty(find(s->contains(s, BLS_RESPONSE_CATALOG_FAIL2), response_json["message"]))
+        catalog_okay = true
+    end
+
+    # Parse response into DataFrames, one for each series
+    n_series = length(response_json["Results"]["series"])
+    for i = 1:n_series
+    end
+
+    return response_json
 end
 
 end # module
