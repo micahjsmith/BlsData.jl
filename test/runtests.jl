79f40d8fae4087ce12c0ab6641080847a3553ab4
Micah Smith <micahjsmith@gmail.com>
2017-05-16 23:51:35 -0400

    Run tests in separate scopes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,20 @@
 using BlsData
 using Base.Test
 
-b0 = Bls()
-b1 = Bls("test")
+let
+    b = Bls()
+    data = get_data(b, "PRS85006092")
+    data = get_data(b, ["PRS85006092", "LNS11000000"])
+    data = get_data(b, "PRS85006092"; startyear=1990)
+    data = get_data(b, ["PRS85006092", "LNS11000000"]; endyear=2014)
+    data = get_data(b, ["PRS85006092", "LNS11000000"]; startyear=1980, endyear=2014)
+    data = get_data(b, "PRS85006092"; catalog=true)
 
-data = get_data(b0, "PRS85006092")
-data = get_data(b0, ["PRS85006092", "LNS11000000"])
-data = get_data(b0, "PRS85006092"; startyear=1990)
-data = get_data(b0, ["PRS85006092", "LNS11000000"]; endyear=2014)
-data = get_data(b0, ["PRS85006092", "LNS11000000"]; startyear=1980, endyear=2014)
-data = get_data(b0, "PRS85006092"; catalog=true)
+    nothing
+end
+
+let
+    b = Bls("test")
+
+    nothing
+end
